f63f3378edd5ea4f0a6c47e4471887ccd540d3a8
James Laurence <jameslaurence82@gmail.com>
2023-10-10 09:05:08 -0300

    updating code to have class/instance method

--- Animal.py
+++ Animal.py
@@ -1,14 +1,29 @@
 # Creation of Animal class
+# The following will be included in the code below
+# __init__ function, class attribute, class method, instance attribute, 
+# instance method, static attribute
 class Animal():
+    
+    species = "" # class attribute
     #__init__ is used to automatically instantiate the Animal class when called
     # Constructor method for Animal class
-    def __init__(self, species, weight, locale): # instance attributes for Animal class  
-        self.species = species # species attribute
-        self.weight = weight # weight attribute
-        self.locale = locale # locale attribute
+    def __init__(self, name, weight, locale): # instance attributes for Animal class  
+        self.name = name # species name instance attribute
+        self.weight = weight # weight instance attribute
+        self.locale = locale # locale instance attribute
 
+    # instance method
     # print attributes method for Animal class
     def details(self):
-        print(f"The Species is: {self.species} and it weighs {self.weight} and it is located in {self.locale}")
+        print(f"The animal is: {self.name} and it weighs {self.weight} and it is located in {self.locale}")
 
+    # Using @classmethod decorator to create a class method
+    @classmethod
+    def species(cls, species):
+        cls.species = species
+        print(f"The animal species is {cls.species}")
+        
+    @staticmethod
+    def gender(gender):
+        print(f"The animal is a {gender}")
 
--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from Shape import Shape # Imports the Shape class from Shape.py
 
 if __name__ == "__main__": # If this file is being run directly
     # The Animal Class hard coded is instantiated
-    horse = Animal("Horse", "1000lbs", "Canada")
-    horse.details()
+    # horse = Animal("Horse", "1000lbs", "Canada")
+    # horse.details()
+    # horse.species("Mammal")
+    # horse.gender("Male")
     
+    frog = Animal("Frog", "1lbs", "Mexico")
+    frog.details()
+    frog.species("Amphibian")
+    frog.gender("Female")
     # The Shape Class hard coded is instantiated
-    sqaure = Shape("square", "red", 300)
-    square.details()
+    # sqaure = Shape("square", "red", 300)
+    # square.details()
     
     # The Animal Class with user input is instantiated
     
